1576d22a74b309a113923bfa3a49daf5763e80fd
MRDK369 <contactme2486@gmail.com>
2023-10-27 13:34:38 +0530

    moving

--- scorce.py
+++ scorce.py
@@ -12,6 +12,7 @@ grass.addshape("body.gif")
 snake=turtle.Turtle()
 snake.penup()
 snake.goto(0,0)
+snake.setheading(90)
 snake.shape("head up.gif")
 
 
@@ -21,4 +22,49 @@ food.color("red")
 food.shape("circle")
 food.goto(100,10)
 
+
+
+def move():
+    snake.forward(5)
+      
+
+
+def up():
+    if snake.heading() !=270:
+        snake.setheading(90)
+        snake.shape("head up.gif")
+   
+def down():
+    if snake.heading() !=90:
+     snake.setheading(270)
+     snake.shape("head down.gif")
+
+def right():
+    if snake.heading() !=180: 
+     snake.setheading(0)    
+    snake.shape("head right.gif")
+
+
+
+def left():
+    if snake.heading() !=0:
+     snake.setheading(180)
+     snake.shape("head left.gif")
+
+
+
+
+turtle.onkeypress(up,"Up")
+turtle.onkeypress(down,"Down")
+turtle.onkeypress(right,"Right")
+turtle.onkeypress(left,"Left")
+turtle.listen()
+
+
+while True:
+   move()
+   
+
+
+   
 turtle.done()
